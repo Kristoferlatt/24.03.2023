64e69307f9d7f154f8b9d9ea1977f894708fa9bc
Kristoferlatt <42769298+Kristoferlatt@users.noreply.github.com>
2023-03-24 13:50:46 +0200

    Add files via upload

---
+++ Pärilusauto/Pärilusauto/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Text.RegularExpressions;
+using System.Text;
+
+namespace Pärilusauto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var masin = new Machine();
+            Console.Write("Sisestage masina number: ");
+            string masinaNumber = Console.ReadLine();
+
+            masin.Pärilus(masinaNumber);
+        }
+        public class Cars
+        {
+            public string vin { get; set; }
+        }
+        public class Machine : Cars
+        {
+            public void Pärilus(string masinaNumber)
+            {
+                string vin = GenerateVinCode(masinaNumber);
+
+                Console.WriteLine("VIN-kood on {0}", vin);
+            }
+
+            private string GenerateVinCode(string masinaNumber)
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
---
+++ ÜL2/ÜL2/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace Polimorfism2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mootorsõidukid");
+
+            Machine machine = new Machine();
+            Machine car = new Car();
+            Machine boat = new Boat();
+
+            machine.MachineSound();
+            car.MachineSound();
+            boat.MachineSound();
+
+            Console.WriteLine();
+        }
+    }
+
+    class Machine
+    {
+        public void MachineSound()
+        {
+            Console.WriteLine("Machine makes sound");
+        }
+    }
+
+    class Car : Machine
+    {
+        public void MachineSound()
+        {
+            Console.WriteLine("Car sounds: PRÕMM PRÕMM");
+        }
+    }
+    class Boat : Machine
+    {
+        public void MachineSound()
+        {
+            Console.WriteLine("Boat sounds: VÕMM VÕMM ");
+        }
+    }
+
+}
